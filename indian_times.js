1b7f648d1e8c6bc4d6adb5266a41dea9c5b9f1b7
Aditya Rathore <145015316+01AdI@users.noreply.github.com>
2026-08-08 21:09:39 +0530

    Update indian_times.js

--- indian_times.js
+++ indian_times.js
@@ -3,9 +3,14 @@ import ReactDom from"react-dom/client";
 import { BrowserRouter, Routes, Route, } from "react-router";
 import Homes from "./src/home";
 import Layout from "./src/Layout";
+import About_Us from "./src/About";
+import CarRental from "./src/CarRental";
 import { useEffect } from "react";
+
+
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Blog from "./src/Blog";
 
 
 function Main(){
@@ -21,10 +26,10 @@ function Main(){
           
           <Route path="/" element={<Layout />}>
             <Route index element={<Homes />} />
-            <Route path="/About-Us" element={<Homes/>} />
+            <Route path="/About_Us" element={<About_Us/>} />
             <Route path="/Categories" element={<Homes />} />
-            <Route path="/Categories/:selected" element={<Homes></Homes>}/>
-            <Route path="/Categories/:selected/:sub" element={<Homes />} />
+            <Route path="/CarRental" element={<CarRental></CarRental>}/>
+            <Route path="/Blog" element={<Blog />} />
           </Route>
 
         </Routes>
